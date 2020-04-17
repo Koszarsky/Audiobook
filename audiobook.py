771284d7e5aa92f2c89e68d0974703f8111e4e77
Koszarsky <49965517+Koszarsky@users.noreply.github.com>
2020-04-16 20:22:56 -0400

    Update audiobook.py

--- audiobook.py
+++ audiobook.py
@@ -1,16 +1,16 @@
-def timeRemaining (hours,mintues,speed):
+def timeRemaining (hours,minutes,speed):
     hoursToMin = hours * 60
-    hoursPlusMin = hoursToMin + mintues
+    hoursPlusMin = hoursToMin + minutes
     speedAlteredTotal = hoursPlusMin / speed
     speedAlteredTotal = int(round(speedAlteredTotal))
     speedAlteredMin = speedAlteredTotal % 60
     speedAlteredHours = (speedAlteredTotal - speedAlteredMin)/60
-    print('At your current speed of ' + str(listenSpeed) + ' you will finish your book in ' + str(speedAlteredHours) + ' hours and ' + str(speedAlteredMin) + ' mintues')
+    print('At your current speed of ' + str(listenSpeed) + ' you will finish your book in ' + str(speedAlteredHours) + ' hours and ' + str(speedAlteredMin) + ' minutes')
 
-print('Welcome!  \nThis program will tell you how much time your have \nleft in your audiobook if you listen to it at a modified speed.')
+print('Welcome!  \nThis program will tell you how much time you have \nleft in your audiobook if you listen to it at a modified speed.')
 print('First, tell me how many hours you have left in your book')
 hoursLeft = int(input())
-print('Great, now can you tell me how many mintues are left?')
+print('Great, now can you tell me how many minutes are left?')
 minLeft = int(input())
 print('Lastly, I need to know what speed you are listening at')
 listenSpeed = float(input())
